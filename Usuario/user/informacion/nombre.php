abcab08664fc4b383eb5b049e6fb4260e2584dff
Manuel Angel Rivera Martinez <anyel_rmtz26@hotmail.com>
2020-10-14 13:58:51 -0500

    actualizacion

---
+++ Usuario/user/informacion/nombre.php
@@ -0,0 +1,16 @@
+<?php 
+require '../../../Conexion/conexion.php';
+$datos=$_GET;
+$clave=$datos['clave'];
+$data='0';
+$Sql="Select Nombre from usuario where clave='$clave'";
+$sentencia=sqlsrv_query($conexion,$Sql);
+if($sentencia===false){
+    dile (print_r(sqlsrv_errora(),true));
+}
+
+while($row=sqlsrv_fetch_Array($sentencia,SQLSRV_FETCH_ASSOC)){
+    echo $row['Nombre'];
+}
+ 
+?>
