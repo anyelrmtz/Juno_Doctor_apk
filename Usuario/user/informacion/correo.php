eeec8d4249fcb6d37a539881fd30cefe16615a17
Manuel Angel Rivera Martinez <anyel_rmtz26@hotmail.com>
2020-10-14 14:09:37 -0500

    actualizacion

--- Usuario/user/informacion/correo.php
+++ Usuario/user/informacion/correo.php
@@ -3,14 +3,14 @@ require '../../../Conexion/conexion.php';
 $datos=$_GET;
 $clave=$datos['clave'];
 $data='0';
-$Sql="Select direccion from usuario where clave='$clave'";
+$Sql="Select correo from usuario where clave='$clave'";
 $sentencia=sqlsrv_query($conexion,$Sql);
 if($sentencia===false){
     dile (print_r(sqlsrv_errora(),true));
 }
 
 while($row=sqlsrv_fetch_Array($sentencia,SQLSRV_FETCH_ASSOC)){
-    echo $row['direccion'];
+    echo $row['correo'];
 }
  
 ?>
