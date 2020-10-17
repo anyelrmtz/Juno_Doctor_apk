25f5a92eb57a9de1e6e246529ad520c9bf794a6d
Manuel Angel Rivera Martinez <anyel_rmtz26@hotmail.com>
2020-10-17 12:53:16 -0500

    actualisar

---
+++ Usuario/busqueda_nino/datos.php
@@ -0,0 +1,15 @@
+<?php 
+require '../../Conexion/conexion.php';
+$datos=$_GET;
+$clave=$datos['clave'];
+$data='0';
+$fechaActual = date('d/m/Y');
+
+$Sql="Select  *from consul_nino where  clave='$clave' and fehca_cita='$fechaActual'";
+$sentencia=sqlsrv_query($conexion,$Sql);
+
+while($row=sqlsrv_fetch_Array($sentencia,SQLSRV_FETCH_ASSOC)){
+    echo "Hora: ".$row['hora_cita']." Fecha: ". $row['fehca_cita']." Doctora: ". $row['doctora']." Consulta: ".$row['tipo_consulta'].'';
+}
+ 
+?>
