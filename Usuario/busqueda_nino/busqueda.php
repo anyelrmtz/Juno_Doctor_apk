ed893d6071e4768f65b839ecfda2fb6024459c3f
Manuel Angel Rivera Martinez <anyel_rmtz26@hotmail.com>
2020-10-17 12:22:18 -0500

    actualizar

--- Usuario/busqueda_nino/busqueda.php
+++ Usuario/busqueda_nino/busqueda.php
@@ -1,13 +1,13 @@
 <?php 
-/*require '../../Conexion/conexion.php';
+require '../../Conexion/conexion.php';
 $datos=$_GET;
 $clave=$datos['clave'];
-$data='0';*/
+$data='0';
 $fechaActual = date('d/m/Y');
 
-echo $fechaActual;
 
-/*$Sql="Select  *from consult_nino where  clave='$clave' and contra='$contra'";
+
+$Sql="Select  *from consult_nino where  clave='$clave' and fehca_cita= like '%$fechaActual'";
 $sentencia=sqlsrv_query($conexion,$Sql);
 if($sentencia===false){
     dile (print_r(sqlsrv_errora(),true));
@@ -17,6 +17,6 @@ if($row=sqlsrv_fetch_Array($sentencia,SQLSRV_FETCH_ASSOC)){
     echo $data='1';
 }else{
     echo $data='0';
-}*/
+}
  
 ?>
