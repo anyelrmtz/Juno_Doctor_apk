395955a03c38bcda58259702e7debbd18ebe27d5
Manuel Angel Rivera Martinez <anyel_rmtz26@hotmail.com>
2020-11-03 16:51:45 -0600

    actualisacion

--- Conexion/conexion.php
+++ Conexion/conexion.php
@@ -1,5 +1,5 @@
 <?php 
-$ServerName="10.23.249.209";
+$ServerName="177.228.214.8";
 $ConectionInfo= array("Database"=>"junodoctor_cliente", "UID"=>"DOCTORJUNIO","PWD"=>"junodoctor2020", 
 "CharacterSet"=>"UTF-8");
 $conexion =sqlsrv_connect($ServerName,$ConectionInfo);
